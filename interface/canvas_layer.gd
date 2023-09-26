593bef407adb0953c467d27cf6a47ef8a39b5426
BragmaDev <mh.bragge@gmail.com>
2023-09-26 03:21:33 +0300

    Add pause menu

--- interface/canvas_layer.gd
+++ interface/canvas_layer.gd
@@ -1,9 +1,18 @@
 extends CanvasLayer
 
 
+onready var pause_menu : Control = $PauseMenu
+
+
 func _ready() -> void:
 	EventBus.connect("sequence_started", self, "_toggle_mouse_blocker", [true])
 	EventBus.connect("sequence_finished", self, "_toggle_mouse_blocker", [false])
+
+
+func _process(_delta : float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pause_menu.set_visible(not pause_menu.is_visible())
+		get_tree().set_pause(pause_menu.is_visible())
 
 
 func _toggle_mouse_blocker(enabled : bool) -> void:
