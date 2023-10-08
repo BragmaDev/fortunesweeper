b0b3a3f7a77544fea175e130cd96780d9df358f1
BragmaDev <mh.bragge@gmail.com>
2023-10-09 01:33:15 +0300

    Add happy animation to player

--- player/player_sprite.gd
+++ player/player_sprite.gd
@@ -5,4 +5,10 @@ onready var anim : AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
-	EventBus.connect("money_decreased", anim, "play", ["shocked"])
+	EventBus.connect("money_decreased", self, "start_animation", ["shocked"])
+	EventBus.connect("money_increased", self, "start_animation", ["happy"])
+
+
+func start_animation(animation_name : String) -> void:
+	anim.stop()
+	anim.play(animation_name)
